2c3b48dab14d075930e8340e2ec7d3a880c01ed5
cn29 <35722345+cn29@users.noreply.github.com>
2018-03-07 12:54:50 -0800

    Add files via upload

--- parse_result.py
+++ parse_result.py
@@ -11,18 +11,31 @@ filelist = os.listdir(Dir)
 print(filelist)
 table = [dict() for _ in range(len(filelist))]
 
+# renaming
+# for i in range(len(filelist)):
+# 	tmp = filelist[i].split('_')
+# 	newname = tmp[0]+'_'+tmp[2][0:3]+'_'+tmp[1]+tmp[2][3:]
+# 	os.rename(Dir+filelist[i], Dir+newname)
+
+# filelist = os.listdir(Dir)
+# print(filelist)filelist = os.listdir(Dir)
+print(filelist)
+
+
+
 for i in range(len(filelist)):
 	with open(Dir+filelist[i], 'r') as f:
 		readcsv = csv.reader(f, delimiter=',')
-		for cnt, row in zip(range(15), readcsv):
+		for cnt, row in zip(range(25), readcsv):
 			if cnt > 0:
 				table[i][row[0]] = "{:.1f}".format(float(row[1])/runtime[i] * 100)
 
-print(table[3])
+
 pprint.pprint(table[1])
+print(type(table[0]['ILUT']))
 
 tableAll = dict()
-for cnt, key in zip(range(6), table[0]):
+for cnt, key in zip(range(10), table[0]):
 	tableAll[key] = []
 	for i in range(len(filelist)):
 		if key in table[i]:
