06b5d3b2d28325f0636dd38031331cea67d7f480
Chen Zhang <czhangcn@beluga.ou.ad3.ucdavis.edu>
2018-04-10 15:17:00 -0700

    new

--- parse_result.py
+++ parse_result.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 # np.set_printoptions(precision=2)
-runtime = [650, 15500, 670, 3000]
+runtime = [565.5, 12806.8, 184.9, 543.1, 2825.3]
 Dir = 'results_csv/'
 filelist = os.listdir(Dir)
 print(filelist)
@@ -65,20 +65,22 @@ npercent = np.asarray(percent)
 npercent_sum = np.sum(npercent, axis=0)
 print(npercent_sum)
 for idx in range(len(top_rountines)):
+	if(top_rountines[idx].startswith('_')):
+		top_rountines[idx]  = 'f'+top_rountines[idx];
 	print(top_rountines[idx], end='\t')
 	print(["{0:.1f}".format(perc) for perc in percent[idx]], end=' ')
 	print(' ')
 
 # plot
-x = [0,1,2,3]
-xlabels = ['SimC2V_CG\n%.1f%%'%npercent_sum[0], 'Butte\n%.1f%%'%npercent_sum[1], 
-			'SimC2V_FG\n 3 years\n%.1f%%'%npercent_sum[2], 'SimC2V_FG\n 15 years\n%.1f%%'%npercent_sum[3]]	
+x = [0,1,2,3,4]
+xlabels = ['SimC2V_CG\n%.1f%%'%npercent_sum[0], 'Butte\n%.1f%%'%npercent_sum[1], 'SimC2V_FG\n 1 years\n%.1f%%'%npercent_sum[2],
+			'SimC2V_FG\n 3 years\n%.1f%%'%npercent_sum[3], 'SimC2V_FG\n 15 years\n%.1f%%'%npercent_sum[4]]
 plt.figure()
 #set x limits
-plt.ylim((-1, 30))
-plt.xlim((-1, 4))
+plt.ylim((-1,25))
+plt.xlim((-0.5, 4.5))
 # set tick labels
-plt.xticks([0, 1, 2, 3], xlabels)
+plt.xticks([0, 1, 2, 3, 4], xlabels)
 
 l0, = plt.plot(x, percent[0], label=top_rountines[0], marker='D')
 l1, = plt.plot(x, percent[1], label=top_rountines[1], marker='D', color='red', linewidth=1.0, linestyle='--')
@@ -88,8 +90,8 @@ l4, = plt.plot(x, percent[4], label=top_rountines[4], marker='D', color='m', lin
 l5, = plt.plot(x, percent[5], label=top_rountines[5], marker='D', color='y', linewidth=1.0, linestyle='--')
 l6, = plt.plot(x, percent[6], label=top_rountines[6], marker='D', color='k', linewidth=1.0, linestyle='--')
 l7, = plt.plot(x, percent[7], label=top_rountines[7], marker='D', color='b', linewidth=1.0, linestyle=':')
-l8, = plt.plot(x, percent[8], label=top_rountines[8], marker='D', color='b', linewidth=1.0, linestyle=':')
-l9, = plt.plot(x, percent[9], label=top_rountines[9], marker='D', color='c', linewidth=1.0, linestyle=':')
+l8, = plt.plot(x, percent[8], label=top_rountines[8], marker='D', color='b', linewidth=1.0, linestyle='-')
+l9, = plt.plot(x, percent[9], label=top_rountines[9], marker='D', color='c', linewidth=1.0, linestyle='-')
 
 
 plt.legend(handles=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9], labels=top_rountines[0:10],  loc='best')
